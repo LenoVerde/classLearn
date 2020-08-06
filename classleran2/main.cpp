3e4a3da2597d7b1329164c158faa2a5247b54050
まちごめ <54085031+LenoVerde@users.noreply.github.com>
2020-08-07 00:53:48 +0900

    test3

---
+++ classleran2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  classleran2
+//
+//  Created by 坂野孝広 on 2020/08/07.
+//  Copyright © 2020 Digital Circus Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
